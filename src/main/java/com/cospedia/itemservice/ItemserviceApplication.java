4e6bb715609a1de7d4a3aa36e440bbbd55ad4e7c
sevdadurdu <sevdadurdu@outlook.com>
2018-08-02 14:27:37 +0300

    Created Model

--- src/main/java/com/cospedia/itemservice/ItemserviceApplication.java
+++ src/main/java/com/cospedia/itemservice/ItemserviceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class ItemserviceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		SpringApplication.run(ItemserviceApplication.class, args);
 	}
 }
