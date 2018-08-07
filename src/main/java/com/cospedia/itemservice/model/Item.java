a49c277bc53998c306022c139f8e34997445c929
Mesut Dogan <mesut.dogan@iconecwave.com>
2018-08-07 19:14:29 +0300

    eurke-server docker integration.

--- src/main/java/com/cospedia/itemservice/model/Item.java
+++ src/main/java/com/cospedia/itemservice/model/Item.java
@@ -1,82 +1,76 @@
 package com.cospedia.itemservice.model;
 
-
 import org.springframework.data.annotation.Id;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 
 public class Item {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	private String id;
 
-    private String name;
+	private String name;
 
-    private String category;
+	private String category;
 
-    private String brand;
+	private String brand;
 
-    private String description;
+	private String description;
 
-    private Double tag;
+	private Double tag;
 
-    private Double rate;
+	private Double rate;
 
-    public Long getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getCategory() {
-        return category;
-    }
+	public String getCategory() {
+		return category;
+	}
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
-    public String getBrand() {
-        return brand;
-    }
+	public String getBrand() {
+		return brand;
+	}
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public Double getTag() {
-        return tag;
-    }
+	public Double getTag() {
+		return tag;
+	}
 
-    public void setTag(Double tag) {
-        this.tag = tag;
-    }
+	public void setTag(Double tag) {
+		this.tag = tag;
+	}
 
-    public Double getRate() {
-        return rate;
-    }
+	public Double getRate() {
+		return rate;
+	}
 
-    public void setRate(Double rate) {
-        this.rate = rate;
-    }
+	public void setRate(Double rate) {
+		this.rate = rate;
+	}
 }
